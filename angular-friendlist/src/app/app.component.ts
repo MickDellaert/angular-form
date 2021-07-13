b32ec37b4fd753a8a93671b56dece2a1d335e2bc
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2021-07-13 12:34:12 +0200

    made the languages dropdown into an array

--- angular-friendlist/src/app/app.component.ts
+++ angular-friendlist/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-friendlist';
+
+  // this array gets used in the select options dropdown in app.component.html
+  languages = [{name: "HTML"}, {name: "CSS"}, {name: "JavaScript"}, {name: "PHP"}]
+
 }

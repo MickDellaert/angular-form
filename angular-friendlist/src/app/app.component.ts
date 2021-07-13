886c1bc6a9dc92b7c4cf738e3ce64d4f1de46960
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2021-07-13 17:45:53 +0200

    validation working

--- angular-friendlist/src/app/app.component.ts
+++ angular-friendlist/src/app/app.component.ts
@@ -7,11 +7,12 @@ import { Friend } from "./friend";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'angular-friendlist';
 
   // this array gets used in the select options dropdown in app.component.html
   languages = [{name: "HTML"}, {name: "CSS"}, {name: "JavaScript"}, {name: "PHP"}]
 
   // instantiate a new friendModel object using the Friend class defined in friend.ts
-  friendModel = new Friend("","","","","")
+  friendModel = new Friend( "", "", "", "", "");
 }
